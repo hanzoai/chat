a5ec39adf9f06ce5e9582c7f3d665a01e89cb8d3
Hanzo Dev <dev@hanzo.ai>
2025-04-12 05:46:35 -0500

    Update brand

--- api/app/clients/OpenAIClient.js
+++ api/app/clients/OpenAIClient.js
@@ -657,7 +657,7 @@ class OpenAIClient extends BaseClient {
       configOptions.baseOptions = {
         headers: {
           'HTTP-Referer': 'https://librechat.ai',
-          'X-Title': 'LibreChat',
+          'X-Title': 'Hanzo',
         },
       };
     }
@@ -1159,7 +1159,7 @@ ${convo}
       if (this.useOpenRouter) {
         opts.defaultHeaders = {
           'HTTP-Referer': 'https://librechat.ai',
-          'X-Title': 'LibreChat',
+          'X-Title': 'Hanzo',
         };
       }
 
